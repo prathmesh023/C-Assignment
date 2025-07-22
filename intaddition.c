4effb9638c051f797b596e90b05c4b424cb79d18
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-07-22 23:01:05 +0530

    Primary data type (integer)

---
+++ intaddition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int iNo1=50;
+  int iNo2=100;
+  int sum=0;
+  
+  printf("print value of iNo1 = %d \n",iNo1);
+  printf("print value of iNo2 = %d \n",iNo2);
+  
+   sum= iNo1 + iNo2;
+   
+   printf("addition of both number is %d\n",sum);
+   
+   return 0;
+   
+}
