804c0e283539711223b9901f78127cc38d81ced5
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-07-31 17:53:08 +0530

    String
    String using fgets and puts

---
+++ string1.c
@@ -0,0 +1,13 @@
+// write program String use user input(only letter print)
+
+#include<stdio.h>
+//#include<string.h>
+ 
+int main()
+{
+
+    char str[50];
+    scanf("%s",str);
+    printf("your entered: %s\n",str);
+    return 0;
+}
