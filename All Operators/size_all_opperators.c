ee8caf640cc6ae681de418bafe94807e79e08756
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-07-31 18:51:13 +0530

    All Opperators
    Size of all opperators

---
+++ All Operators/size_all_opperators.c
@@ -0,0 +1,21 @@
+//memory size of int,float,double,char,short int,long int,long double at one program
+
+#include<stdio.h>
+int main()
+{
+    
+    printf("size of integer is: %ld\n",sizeof(int));
+
+    printf("size of float is: %ld\n",sizeof(float));
+
+    printf("size of double is: %ld\n",sizeof(double));
+
+    printf("size of char is: %ld\n",sizeof(char));
+
+    printf("size of short int is: %ld\n",sizeof(short int));
+
+    printf("size of long int is: %ld\n",sizeof(long int));
+   
+    printf("size of long double is: %ld\n",sizeof(long double));
+
+}
